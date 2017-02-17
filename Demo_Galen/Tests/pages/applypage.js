91554169ca6b1421aea5c927ec7156287ff9d034
Infobeans-Anujita <anujita.sinha@infobeans.com>
2017-02-17 11:18:48 +0530

    added other test case

---
+++ Demo_Galen/Tests/pages/applypage.js
@@ -0,0 +1,16 @@
+
+this.applynowpage = $page("applynowpage",{
+  menuApply: "#menu-item-1750>a" ,
+	firstname: "xpath: .//input[@id='Ffirstname']" ,
+	lastname: "xpath: .//input[@id='Flastname']" ,
+	email: "xpath: .//*[@id='wpcf7-f2120-p40-o1']/form/ul/li[3]/span/input" ,
+	phone: "xpath: .//*[@id='wpcf7-f2120-p40-o1']/form/ul/li[4]/span/input" ,
+	applybutton: "xpath: .//*[@id='submit-wrap']/input[1]" ,
+});
+var applynowPage = new applynowpage(driver);
+applynowPage.firstname.typeText("Anujita");
+applynowPage.lastname.typeText("Sinha");
+applynowPage.email.typeText("anujita022@gmail.com");
+applynowPage.phone.typeText("1234567890");
+applynowPage.applybutton.click();
+	
---
+++ Demo_Galen/Tests/pages/home.js
@@ -0,0 +1,9 @@
+this.homepage = $page("homepage", {
+	toggleButton: "id: toggle" ,
+	menuApply: "#menu-item-1750>a" ,
+});
+var homePage = new homepage(driver);
+homePage.toggleButton.click();
+homePage.menuApply.click();
+
+
