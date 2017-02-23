b63387b3f6b000256d80a385369fcc22ec8ed041
Infobeans-Anujita <anujita.sinha@infobeans.com>
2017-02-23 11:19:50 +0530

    removed the commented lines if any

--- Demo_Galen/Tests/pages/home.js
+++ Demo_Galen/Tests/pages/home.js
@@ -1,9 +1,5 @@
 this.homepage = $page("homepage", {
 	toggleButton: "id: toggle" ,
-	// menuApply: "#menu-item-1750>a" ,
 });
 var homePage = new homepage(driver);
 homePage.toggleButton.click();
-// homePage.menuApply.click();
-
-
